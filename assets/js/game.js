f58901993bc876ffc88202bd9c799e87eb22d8a9
Donnie Heard <dhearddev@gmail.com>
2018-06-14 22:36:37 -0700

    v1.0 Release

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,86 +1,114 @@
 $(document).ready(function(){
     
     let intervalId = undefined;
-    let timelimit = 90000; //timelimit in ms
-    let question_idx = 0;
+    let timeLimit = 0; //timelimit in seconds
+    let unanswered_ans = 0;
     let incorrect_ans = 0;
     let correct_ans = 0;
+
     let QAList = [{
         question: "Which Marvel Character speaks in a Cajun Accent, and originates from Lousiana?",
-        answers: {
-            ans1: "Wolverine",
-            ans2: "Jubilee",
-            ans3: "Mister Sinister",
-            ans4: "Gambit"
-        },
-        correct_ans: 4
+        answers: ["Wolverine","Jubilee","Mister Sinister","Gambit"],
+        correct_ans: "Gambit"
     },
     {
         question: "The U.N.'s counter terrorism agency is called 'S.H.E.I.L.D'. What is Britain counter terrorism agency?",
-        answers: {
-            ans1: "B.L.A.D.E",
-            ans2: "S.T.R.I.K.E",
-            ans3: "H.Y.D.R.A",
-            ans4: "D.E.S.T.I.N.Y"
-        },
-        corret_ans: 2 
+        answers: ["B.L.A.D.E","S.T.R.I.K.E","H.Y.D.R.A","D.E.S.T.I.N.Y"],
+        correct_ans: "S.T.R.I.K.E"
     },
     {
         question: "Which love interest of DareDevil lead the ancient originization known as 'The Hand'?",
-        answers: {
-            ans1: "Elektra",
-            ans2: "Stick",
-            ans3: "Storm",
-            ans4: "Typhoid Mary"
-        },
-        correct_ans: 1
+        answers: ["Elektra","Stick","Storm","Typhoid Mary"],
+        correct_ans: "Elektra"
     }]
 
     function displayQuestionsAnswers(){
-        $('#questions').text(QAList[question_idx].question);
-        
-        let answerList = QAList[question_idx].answers,
-            answerRadio = $('<form>');
-        
-        answerList.forEach(function(ans,idx) {
-            let inputElem = $(`<input class='answer-${idx}'>`).text(ans);
-            answerRadio.append(inputElem); 
+        QAList.forEach(function(elem,idx) {
+            // query #question-section
+            let qaSection = $('.qa-section');
+            // create new div with class questions
+            let questionDiv = $("<div class='questions'>");
+            questionDiv.text(elem.question);
+            // create new div with class answers
+            let answersDiv = $(`<div class='answers-${idx}'>`);
+            // create new form with for each answer
+            let ansArr = elem.answers;
+            let answerForm = $('<form>');
+            ansArr.forEach(function(ans,idx) {
+                let noSpaceStr = ans.replace(/\s/g,'');
+                answerForm.append(`<input type="radio" name="answer" value=${noSpaceStr}>${ans}`);
+            })
+
+            answersDiv.append(answerForm);
+            qaSection.append(questionDiv);
+            qaSection.append(answersDiv);
         })
-        $('#answers').append(answerRadio);
+    }
+
+    function calcScore() {
+        QAList.forEach(function(qaItem,idx) {
+            console.log($(`.answers-${idx} input:checked`));
+            let checked_ans = $(`.answers-${idx} input:checked`).val();
+            let item_ans = qaItem.correct_ans.replace(/\s/g,'');
+
+            console.log(`qaItem-${idx}: ${item_ans}`);
+            console.log(`checked_ans: ${checked_ans}`);
+            
+            if(item_ans === checked_ans)
+                ++correct_ans;
+            else if(checked_ans == undefined)
+                ++unanswered_ans;
+            else
+                ++incorrect_ans;
+        })
+    }
+
+    function checkInput(){
+        $('#gameBtn').off('click').on('click',function() {
+            calcScore();
+            displayResults();
+        })
     }
 
     function displayResults() {
-        //TODO: Display Correct/Incorrect Answers
+        clearInterval(intervalId);
+        $('#timeLimit').text('Here are the Results:');
+        
+        let qaSectionElem = $('.qa-section');
+        qaSectionElem.empty();
+
+        let correctAnsElem = $('<p>').text(`Correct Answers: ${correct_ans}`);
+        let incorrectAnsElem =  $('<p>').text(`Incorrect Answers: ${incorrect_ans}`);
+        let unansweredElem = $('<p>').text(`Unanswered: ${unanswered_ans}`);
+
+        qaSectionElem.append(correctAnsElem);
+        qaSectionElem.append(incorrectAnsElem);
+        qaSectionElem.append(unansweredElem);
+
+        $('#gameBtn').text("Play Again");
+        $('#gameBtn').off('click').on('click',function() {
+            qaSectionElem.empty();
+            startGame();    
+        })
     }
 
     function resetGame(){
-        intervalId = undefined;
-        timelimit = 90;
-        question_idx = 0;
+        $('#gameBtn').text('Finished');
+        $('#timeLimit').text('GAME ON!');
+        clearInterval(intervalId);
+        timeLimit = 90;
         correct_ans = 0;
         incorrect_ans = 0;
+        unanswered_ans = 0;
     }
 
     function timeLeft() {
-        if(timelimit > 0)
-        {
-            //TODO: Display Time Left
+        if(timeLimit > 0){
             $('#timeLimit').text(`Time Left: ${timeLimit}`);
-            //TODO: Decrement Timelimit by 1
-            timelimit -= 1;
+            timeLimit -= 1;
         }
-        else
-        {
-            //TODO: Display Time's Up!
-            $('#timeLimit').text("TIME'S UP!");
-            //TODO: Move to Next Question if Index < Questions.Length
-            if(question_idx < QAList.lenth){
-                ++question_idx
-            }
-            //TODO: Else Stop Interval and Display Game Over Screen 
-            else {
-
-            }
+        else{
+            displayResults();  
         }
     }
 
@@ -88,9 +116,8 @@ $(document).ready(function(){
     {
         resetGame();
         displayQuestionsAnswers();
-        
-        //Start Interval Timer
         intervalId = window.setInterval(timeLeft,1000);
+        checkInput();
     }
 
     startGame();
